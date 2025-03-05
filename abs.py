c716502b7ac93146ea14c24cb76d316dae4bb175
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-03-05 16:59:24 +0530

    Create abs.py
    abs function

---
+++ abs.py
@@ -0,0 +1,9 @@
+no = 24
+print(" identifier :", no)
+print("abs(no) :", abs(no))
+no = - 60
+print("abs(no):", abs(no))
+no = 33.255
+print("abs(no):", abs(no))
+no = -6.8
+print("abs(no):", abs(no))
