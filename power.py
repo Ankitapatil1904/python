1798444af9a564b71d048250bab47182757a9203
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-03-06 11:59:16 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,27 @@
+n1 = 5
+n2 = 3
+print("Identifier n1 :", n1, "and n2 :", n2)
+print("pow(n1, n2) :", pow(n1, n2))
+n1, n2 = 5.0, 3.0
+print("Identifier n1 :", n1, "and n2 :", n2)
+print("pow(n1, n2) :", pow(n1, n2))
+print("pow(7, 2) :", pow(7, 2))
+print("pow(7.0, 2) :", pow(7.0, 2))
+print("pow(7, 2.0) :", pow(7, 2.0))
+print("pow(5, 0) :", pow(5, 0))
+print("pow(7, 0.0) :", pow(7, 0.0))
+print("\nUsing the math.pow() function :")
+n1 = 5
+n2 = 3
+import math
+print("Identifier n1 :", n1, "and n2 :", n2)
+print("math.pow(n1, n2) :", math.pow(n1, n2))
+n1, n2 = 5.0, 3.0
+print("Identifier n1 :", n1, "and n2 :", n2)
+print("math.pow(n1, n2) :", math.pow(n1, n2))
+print("math.pow(7, 2) :", math.pow(7, 2))
+print("math.pow(7.0, 2) :", math.pow(7.0, 2))
+print("math.pow(7, 2.0) :", math.pow(7, 2.0))
+print("math.pow(5, 0) :", math.pow(5, 0))
+print("math.pow(7, 0.0) :", math.pow(7, 0.0))
+print("\nThank you.")
