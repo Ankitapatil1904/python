163ac133f20aa2d66c7647d95444e8a8626f53bc
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-03-15 16:12:13 +0530

    Create boolean.py
    bollean

---
+++ boolean.py
@@ -0,0 +1,9 @@
+n1 , n2 = 20 , 40
+print("n1:",n1 ,"n2:", n2)
+print("n1 > n2 :", n1 > n2)
+print("type of output :",type(n1 > n2))
+n1 , n2 = -20, -40
+print("after setting new values:")
+print("n1 :", n1 ,"n2:",n2)
+print("n1 > n2 :", n1 > n2)
+print("type of output :",type(n1 > n2))
