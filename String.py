1c7a981ac984629a0173f01fab4697c625e944ae
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-03-16 23:56:47 +0530

    Create String.py

---
+++ String.py
@@ -0,0 +1,10 @@
+print("\t -- String identifiers, values and their IDs --\n")
+s1 = "Python"
+print("Identifier s1 :", s1, "Type :", type(s1), "ID :", id(s1))
+s2 = 'Python'
+print("Identifier s2 :", s2, "Type :", type(s2), "ID :", id(s2))
+s3 = 'python'
+print("Identifier s3 :", s3, "Type :", type(s3), "ID :", id(s3))
+s4 = s3
+print("Identifier s4 :", s4, "Type :", type(s4), "ID :", id(s4))
+print("\nThank you.")
