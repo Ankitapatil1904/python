9127ab04ba01a8093dd74c258c3f96637e8ec391
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-03-18 20:28:46 +0530

    Create len.py

---
+++ len.py
@@ -0,0 +1,10 @@
+list1 = [ 2 , 4 , 6 , 8 , 10]
+print("identifier list1:" , list1)
+print("data type :" , type(list1))
+print("number of elements in listl1:", len(list1))
+list2 = [ "red",9999 , True , 3.14 , 3, 'python', "list of words " , 1+12j]
+print("identifier list 2 :" , list2)
+print("data type :",type(list2))
+print("number of elements in list listl2:" , len(list2))
+
+
