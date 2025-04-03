199cb90434aba91b1db2a55fd147b30662dda7aa
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-04-03 12:19:20 +0530

    Create multipleTupleObject.py

---
+++ multipleTupleObject.py
@@ -0,0 +1,6 @@
+t1 = ('Python', 123, 3.14, 12+34J, True, -240, 0, 11.22)
+print("Identifier t1 :", t1, "Type :", type(t1), "ID :", id(t1))
+t2 = ('Python', 123, 3.14, 12+34J, True, -240, 0, 11.22)
+print("Identifier t2 :", t2, "Type :", type(t2), "ID :", id(t2))
+t3 = t2
+print("Identifier t3 :", t3, "Type :", type(t3), "ID :", id(t3))
