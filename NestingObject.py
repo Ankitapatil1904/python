01569606e307c3e2061800dbb2add2eb5fc54a70
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-04-04 13:24:29 +0530

    Create NestingObject.py

---
+++ NestingObject.py
@@ -0,0 +1,10 @@
+s = "string"
+f = 3.14
+b = True
+c = 12.34j
+l =["list", 2 , -4.5 , False , 5.6+78.90j]
+t = ("list", 2 , -4.5 , False , 5.6+78.90j)
+d= ("rollno:" , 5 ,"name :" , "gajanan:", "college", "wit:", "solapur:")
+a = (2 , 4 ,6 , 8 ,10)
+t1 = ( s , f , b , c , l , t , d , a)
+print("identifier:" , t1 , "type:" , type(t1) , "id:" , id(t1) , "length:" , len(t1))
