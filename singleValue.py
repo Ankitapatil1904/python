1755be66842d0bf7a84333b486708a115a9c24cd
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2025-04-04 12:51:15 +0530

    Create singleValue.py

---
+++ singleValue.py
@@ -0,0 +1,17 @@
+s1 = "python"
+print("identifier :", s1 ," type: ", type(s1),"id:", id(s1),"length:", len(s1))
+t1=(s1 ,)
+print("identifier :", t1 ," type: ", type(t1),"id:", id(t1),"length:", len(t1))
+s2 = 444
+print("identifier :", s2 ," type: ", type(s2),"id:", id(s2))
+t2=(s2 ,)
+print("identifier :", t2 ," type: ", type(t2),"id:", id(t2))
+s3 = 9.99
+print("identifier :", s3 ," type: ", type(s3),"id:", id(s3))
+t3=(s3 ,)
+print("identifier :", t3 ," type: ", type(t3),"id:", id(t3))
+s4= 4+7j
+print("identifier :", s4," type: ", type(s4),"id:", id(s4))
+t4=(s4 ,)
+print("identifier :", t4 ," type: ", type(t4),"id:", id(t4))
+
